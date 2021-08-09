5c0dc39c8216e8365a838e10b46ed1b55ac7f1c9
Alpha17-2 <alphaisgod1@gmail.com>
2021-08-10 00:04:31 +0530

    New widget - GestureDetector

---
+++ lib/Widgets/GestureDetector_Widget.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widgets/helper/DeviceSize.dart';
+import 'package:flutter_widgets/helper/helpingMethods.dart';
+
+class GestureDetector_Widget extends StatelessWidget {
+  SnackBar snackBar = SnackBar(
+    content: Text('Successful', style: TextStyle(color: Colors.white)),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        title: Text('Gesture Detector Widget'),
+      ),
+      body: Container(
+        height: displayHeight(context),
+        width: displayWidth(context),
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: displayHeight(context) * 0.16,
+              width: displayWidth(context),
+              // color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                //crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Tap me',
+                          style: TextStyle(
+                              fontSize: displayWidth(context) * 0.045),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Opacity(
+                    opacity: 0.0,
+                    child: Divider(),
+                  ),
+                  Text('Single click Gesture')
+                ],
+              ),
+            ),
+            Container(
+                height: displayHeight(context) * 0.006,
+                width: displayWidth(context),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                  child: linebreakAnimation(context),
+                )),
+            Container(
+              height: displayHeight(context) * 0.16,
+              width: displayWidth(context),
+              // color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                //crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onLongPress: () {
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Long Press Me',
+                          style: TextStyle(
+                              fontSize: displayWidth(context) * 0.045),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Opacity(
+                    opacity: 0.0,
+                    child: Divider(),
+                  ),
+                  Text('Long Press Gesture')
+                ],
+              ),
+            ),
+            Container(
+                height: displayHeight(context) * 0.006,
+                width: displayWidth(context),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                  child: linebreakAnimation(context),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/helper/helpingMethods.dart
+++ lib/helper/helpingMethods.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_widgets/helper/DeviceSize.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 redirectToCode(String url) async {
@@ -6,4 +9,28 @@ redirectToCode(String url) async {
   } else {
     throw 'Could not launch $url';
   }
+}
+
+Widget linebreakAnimation(context){
+
+  return ListView.builder(
+
+    scrollDirection: Axis.horizontal,
+    itemBuilder: (context, index) {
+    return Padding(
+      padding: const EdgeInsets.only(right:8.0),
+      child: Container(
+        height: displayHeight(context)*0.02,
+        width: displayWidth(context)*0.15,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.black87,
+        ),
+      ),
+    );
+  },itemCount: 6,
+  );
+
+
+
 }
