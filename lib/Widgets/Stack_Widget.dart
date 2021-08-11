c8c68feb31e0e30184ea8d91a9e89f4c0103dd1f
Prabhneet Singh Narula <imailcode@gmail.com>
2021-08-12 01:10:16 +0530

    Implementation of Stack Widget.

--- lib/Widgets/Stack_Widget.dart
+++ lib/Widgets/Stack_Widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../helper/DeviceSize.dart';
 
 class Stack_Widget extends StatelessWidget {
   @override
@@ -6,6 +7,65 @@ class Stack_Widget extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Stack Widget'),
+        backgroundColor: Colors.black87,
+      ),
+      body: Container(
+        height: displayHeight(context),
+        width: displayWidth(context),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                height: displayHeight(context) * 0.35,
+                width: displayWidth(context) * 0.7,
+                decoration: BoxDecoration(
+                  color: Colors.blue[300],
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: Colors.black87,
+                    width: 2,
+                  ),
+                ),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Positioned(
+                      child: Container(
+                        height: displayHeight(context) * 0.30,
+                        width: displayWidth(context) * 0.6,
+                        decoration: BoxDecoration(
+                            color: Colors.amber[200],
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(
+                              color: Colors.black87,
+                              width: 2,
+                            )),
+                      ),
+                    ),
+                    Positioned(
+                      child: Container(
+                          height: displayHeight(context) * 0.25,
+                          width: displayWidth(context) * 0.5,
+                          decoration: BoxDecoration(
+                              color: Colors.indigo[200],
+                              borderRadius: BorderRadius.circular(15),
+                              border: Border.all(
+                                color: Colors.black87,
+                                width: 2,
+                              )),
+                          child: Center(
+                              child: Text(
+                            'This is how the Stack Widget Works!',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ))),
+                    ),
+                  ],
+                )),
+          ],
+        ),
+        // color: Colors.white,
       ),
     );
   }
